7bbbdbacbe235fcb3186419e9a0aa53ab714724f
Ralph <Ralphy.t.small@gmail.com>
2023-11-06 19:49:35 +0000

    Random

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from src.Table import *
-import CardManagement
-import Visual
+from src.Table import Table
+from src.CardManagement import CardManagement
+# import Visual
 
 Table = Table.Table(CardManagement.Shoe(1))
-GUI = Visual.Visuals(Table)
+# GUI = Visual.Visuals(Table)
+
+print(Table)
 
 # to do -make actually functions for the game - add more configuration
