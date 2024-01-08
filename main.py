c0f219dac18033e353b62078bb91802143d8e9ec
Ralph <Ralphy.t.small@gmail.com>
2024-01-08 22:50:18 +0000

    Update for visuals

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from src import Table
 from src import CardManagement
+from src import Visual
 
-# from src import Visual
+
 Table = Table.Table(1)
-Table.createPlayer("Ralph")
-Table.createPlayer("Mang Ye")
-Table.createPlayer("Gabby")
-Table.createPlayer("Palak")
+# Table.createPlayer("Ralph")
+# Table.createPlayer("Mang Ye")
+# Table.createPlayer("Gabby")
+# Table.createPlayer("Palak")
 
-# GUI = Visual.Visuals()
+GUI = Visual.Visuals(Table)
+GUI.attributes("-fullscreen", False)
+GUI.mainloop()
 
-gameRound = Table.startRound()
+# gameRound = Table.startRound()
 
-Table.getPlayers()[0].placeBet(50, 0)
-Table.getPlayers()[1].placeBet(30, 0)
-Table.getPlayers()[2].placeBet(70, 0)
-Table.getPlayers()[0].fold()
-Table.getPlayers()[1].check()
-Table.getPlayers()[2].placeBet(100, 0)
-Table.getPlayers()[0].fold()
-Table.getPlayers()[1].placeBet(300,0)
-gameRound.calcWinner()
+# Table.getPlayers()[0].placeBet(50, 0)
+# Table.getPlayers()[1].placeBet(30, 0)
+# Table.getPlayers()[2].placeBet(70, 0)
+# Table.getPlayers()[0].fold()
+# Table.getPlayers()[1].check()
+# Table.getPlayers()[2].placeBet(100, 0)
+# Table.getPlayers()[0].fold()
+# Table.getPlayers()[1].placeBet(300,0)
+# gameRound.calcWinner()
--- src/Visual.py
+++ src/Visual.py
@@ -1,16 +1,47 @@
 import customtkinter as ctk
-
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
 
 class Visuals(ctk.CTk):
 
-    def __init__(self):
+    def __init__(self,table):
         super().__init__()
         self._player = None
-        self._players = []
         self._testMode = False
+        self.table = table
         self.title("Poker")
-        self.geometry("600x500")
-        self.frame = ctk.CTkFrame()
+        self.geometry("1440x810")
+
+        self.settingsButton = settingsButton(self,text="settings")
+        self.settingsButton.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="sw")
+        
+        self.actionBarFrame = ctk.CTkFrame(self,width=360)
+        self.actionBarFrame.grid(row=3, column=1,columnspan=2, padx=100, pady=(20, 10), sticky="s")
+        self.foldButton = ctk.CTkButton(self.actionBarFrame)
+        self.foldButton.grid(row=0, column=0, pady=(10, 0), sticky="")
+        self.betButton = ctk.CTkButton(self.actionBarFrame)
+        self.betButton.grid(row=0, column=1, pady=(10, 0), sticky="")
+        self.checkButton = ctk.CTkButton(self.actionBarFrame)
+        self.checkButton.grid(row=0, column=2, pady=(10, 0), sticky="")
+        
 
     def enterPlayer(self):
         pass
+
+class settingsButton(ctk.CTkButton):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        
+
+    def callBack(self):
+        print("worked")
+
+class actionBar(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        pass
+
+
+
+GUI = Visuals(None)
+GUI.attributes("-fullscreen", False)
+GUI.mainloop()
