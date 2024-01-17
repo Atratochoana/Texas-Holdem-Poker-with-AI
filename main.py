7c796a9faaf1842541699d0151deea1a58413823
Atratochoana <Ralphy.t.small@gmail.com>
2024-01-17 11:08:11 +0000

    Testings/Evidence

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src import Table
 from src import CardManagement
 from src import Visual
 
-
 Table = Table.Table(1)
 # Table.createPlayer("Ralph")
 # Table.createPlayer("Mang Ye")
@@ -13,7 +12,7 @@ GUI = Visual.Visuals(Table)
 GUI.attributes("-fullscreen", False)
 GUI.mainloop()
 
-# gameRound = Table.startRound()
+gameRound = Table.startRound()
 
 # Table.getPlayers()[0].placeBet(50, 0)
 # Table.getPlayers()[1].placeBet(30, 0)
@@ -23,4 +22,13 @@ GUI.mainloop()
 # Table.getPlayers()[2].placeBet(100, 0)
 # Table.getPlayers()[0].fold()
 # Table.getPlayers()[1].placeBet(300,0)
-# gameRound.calcWinner()
+# testHand = [
+#     CardManagement.Card("spades",14,"Ace",None),
+#     CardManagement.Card("Heart",14,"Ace",None),
+#     CardManagement.Card("Heart",14,"Ace",None),
+#     CardManagement.Card("Heart",12,"Ace",None),
+#     CardManagement.Card("Heart",12,"Ace",None),
+#     CardManagement.Card("Heart",12,"Ace",None),
+#     CardManagement.Card("Heart",9,"Ace",None),
+#            ]
+
