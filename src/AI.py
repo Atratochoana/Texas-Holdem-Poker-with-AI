e2d68f7df9af09570f553e8f2bf74ddeb40c6221
Ralph <Ralphy.t.small@gmail.com>
2024-02-27 18:06:25 +0000

    Chance for two pairs

--- src/AI.py
+++ src/AI.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def evaluateHand(communityCards, player):
 
     hand = player._hand
@@ -8,6 +10,9 @@ def evaluateHand(communityCards, player):
     allCards.append(hand[1])
     comVal = []
     comSuit = []
+    handPair = False
+    cardLen = len(player._gameRound._shoe._decks) * 52
+    
 
     for card in comCards:
         if card == None or card in allCards:
@@ -41,25 +46,76 @@ def evaluateHand(communityCards, player):
         "3": 2,
         "2": 2,
     }
+    chance = {
+        "RF": 0,
+        "SF": 0,
+        "4OfKind": 0,
+        "FullHouse": 0,
+        "Flush": 0,
+        "Straight": 0,
+        "ThreeOfKind": 0,
+        "TwoPair": 0,
+        "Pair": [0,0],
+        "High": 0
+    }
+
+
+
+
+
+    
 
     for x in range(len(evalHand), 0, -1):
         if evalHand[evalIndex[x - 1]] != []:
             handVal += cardVals[str(evalHand[evalIndex[x - 1]][0])] * x
 
     #how close to getting pair
-
+    tempIndex = 0
+    
     if hand[0]._value == hand[1]._value:
+        handPair = True
         print("Pair hand")
     for card in hand:
         if card._value in comVal:
+            chance["Pair"][tempIndex] = 100
             print(f"{card._name} Pair")
         else:
             numVal = comVal.count(card._value)
             numVal = 4 - hand.count(card._value) - numVal
-            print(f"{card._name} Pair chance: {numVal/((len(player._gameRound._shoe._decks)*52)-2-len(comVal))*100}")
-
+            chance["Pair"][tempIndex] = numVal/((len(player._gameRound._shoe._decks)*52)-2-len(comVal))*100
+            print(f"{card._name} Pair chance: {numVal/(cardLen-2-len(comVal))*100}")
+        tempIndex += 1
+        
     #chance to two
+    pairs = 0
+    if handPair == True:
+        for val in comVal:
+            if comVal.count(val) >= 2:
+                chance["TwoPair"] = 100
+
+        if chance["TwoPair"] != 100:
+            chance["TwoPair"] = len(np.unique(comVal))/(cardLen - len(comVal) - 2)
+    else:
+        for card in comVal:
+            for hcard in hand: 
+                if card == hcard._value: #checks if card is a pair with a card in the hand
+                    pairs += 1
+            if comVal.count(card) >= 2:
+                pairs += 0.5
+
+        if pairs == 1:
+            chance["TwoPair"] = (numVal + (3 * len(np.unique(comVal))) /(cardLen-2-len(comVal))*100)
+        if pairs == 0:
+            chance["TwoPair"] =  ((3 + len(comVal) / (cardLen - len(comVal) - 2)) ** 2) * 100
+
+        if pairs == 2:
+            chance["TwoPair"] = 100
+                
+    print(int(pairs))
+    
+    
     
     print(evalHand)
     print("End:", handVal)
+    print(chance)
     return
